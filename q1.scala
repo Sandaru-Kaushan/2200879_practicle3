afed17f108acfe822f78da6052b95ea2a1603213
Sandaru-Kaushan <sandarukaushan1560@gmail.com>
2024-07-15 23:02:40 +0530

    prac5q1

--- q1.scala
+++ q1.scala
@@ -1,4 +1,35 @@
-def reverseString(s: String): String = {
-  if (s.isEmpty) "" 
-  else reverseString(s.tail) + s.head
+import scala.io.StdIn.readLine
+
+object InventoryManagementSystem {
+
+  def main(args: Array[String]): Unit = {
+    val products = getProductList()
+    printProductList(products)
+    println(s"Total number of products: ${getTotalProducts(products)}")
+  }
+
+  def getProductList(): List[String] = {
+    var productList: List[String] = List()
+    var input: String = ""
+
+    println("Enter product names. Type 'done' to finish:")
+
+    input = readLine()
+    while (input.toLowerCase != "done") {
+      productList = productList :+ input
+      input = readLine()
+    }
+
+    productList
+  }
+
+  def printProductList(products: List[String]): Unit = {
+    products.zipWithIndex.foreach { case (product, index) =>
+      println(s"${index + 1}. $product")
+    }
+  }
+
+  def getTotalProducts(products: List[String]): Int = {
+    products.length
+  }
 }
