2785dee58a31abe954ce74ee13517ee25598955b
Sandaru-Kaushan <sandarukaushan1560@gmail.com>
2024-08-06 00:51:19 +0530

    practical7 question1

--- q1.scala
+++ q1.scala
@@ -1,35 +1,11 @@
-import scala.io.StdIn.readLine
-
-object InventoryManagementSystem {
+object FilterEvenNumbersApp {
+  def filterEvenNumbers(numbers: List[Int]): List[Int] = {
+    numbers.filter(num => num % 2 == 0)
+  }
 
   def main(args: Array[String]): Unit = {
-    val products = getProductList()
-    printProductList(products)
-    println(s"Total number of products: ${getTotalProducts(products)}")
-  }
-
-  def getProductList(): List[String] = {
-    var productList: List[String] = List()
-    var input: String = ""
-
-    println("Enter product names. Type 'done' to finish:")
-
-    input = readLine()
-    while (input.toLowerCase != "done") {
-      productList = productList :+ input
-      input = readLine()
-    }
-
-    productList
-  }
-
-  def printProductList(products: List[String]): Unit = {
-    products.zipWithIndex.foreach { case (product, index) =>
-      println(s"${index + 1}. $product")
-    }
-  }
-
-  def getTotalProducts(products: List[String]): Int = {
-    products.length
+    val input = List(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+    val output = filterEvenNumbers(input)
+    println(output)
   }
 }
