b752edb0daac1924d0acafc0159bf3f3bb555ac2
Sandaru-Kaushan <sandarukaushan1560@gmail.com>
2024-09-10 08:31:22 +0530

    Practical 10 Question 3

--- q3.scala
+++ q3.scala
@@ -1,17 +1,50 @@
-object filterPrime{
-def isPrime(n: Int): Boolean = {
-  if (n <= 1) false
-  else if (n == 2) true
-  else !(2 until n).exists(i => n % i == 0)
+class Account(val accountNumber: String, private var balance: Double) {
+
+  def deposit(amount: Double): Unit = {
+    if (amount > 0) {
+      balance += amount
+      println(s"Deposited $$${amount}. Current balance: $$${balance}")
+    } else {
+      println("Deposit amount must be positive")
+    }
+  }
+
+  def withdraw(amount: Double): Unit = {
+    if (amount > 0 && amount <= balance) {
+      balance -= amount
+      println(s"Withdrew $$${amount}. Current balance: $$${balance}")
+    } else if (amount > balance) {
+      println("Insufficient balance")
+    } else {
+      println("Withdraw amount must be positive")
+    }
+  }
+
+  def transfer(amount: Double, toAccount: Account): Unit = {
+    if (amount > 0 && amount <= balance) {
+      withdraw(amount)  
+      toAccount.deposit(amount)  
+      println(s"Transferred $$${amount} from account ${accountNumber} to account ${toAccount.accountNumber}")
+    } else if (amount > balance) {
+      println("Insufficient balance to transfer")
+    } else {
+      println("Transfer amount must be positive")
+    }
+  }
+
+  def getBalance: Double = balance
 }
 
-def filterPrime(numbers: List[Int]): List[Int] = {
-  numbers.filter(num => isPrime(num))
-}
+object BankApp {
+  def main(args: Array[String]): Unit = {
+    val account1 = new Account("123456", 1000.0)
+    val account2 = new Account("654321", 500.0)
 
-def main(args:Array[String]):Unit={
-val input = List(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
-val output = filterPrime(input)
-println(output) // Output: List(2, 3, 5, 7)
-}
+    account1.deposit(200)
+    account1.withdraw(150)
+    account1.transfer(300, account2)
+
+    println(s"Final balance of account1: $$${account1.getBalance}")
+    println(s"Final balance of account2: $$${account2.getBalance}")
+  }
 }
