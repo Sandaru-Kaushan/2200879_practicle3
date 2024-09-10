d7cab68134ac2fa48bd2dafc38ccb49fee32636c
Sandaru-Kaushan <sandarukaushan1560@gmail.com>
2024-09-10 08:29:09 +0530

    practical 10 question1

--- q1.scala
+++ q1.scala
@@ -1,11 +1,46 @@
-object FilterEvenNumbersApp {
-  def filterEvenNumbers(numbers: List[Int]): List[Int] = {
-    numbers.filter(num => num % 2 == 0)
-  }
+class Rational(n: Int, d: Int) {
+  require(d != 0, "Denominator must be non-zero")
 
-  def main(args: Array[String]): Unit = {
-    val input = List(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
-    val output = filterEvenNumbers(input)
-    println(output)
-  }
+  private val g = gcd(n.abs, d.abs)
+  val numer: Int = n / g
+  val denom: Int = d / g
+
+  def this(n: Int) = this(n, 1)
+
+  def neg: Rational = new Rational(-numer, denom)
+
+  def +(that: Rational): Rational =
+    new Rational(
+      numer * that.denom + that.numer * denom,
+      denom * that.denom
+    )
+
+  def -(that: Rational): Rational =
+    new Rational(
+      numer * that.denom - that.numer * denom,
+      denom * that.denom
+    )
+
+  def *(that: Rational): Rational =
+    new Rational(numer * that.numer, denom * that.denom)
+
+  def /(that: Rational): Rational =
+    new Rational(numer * that.denom, denom * that.numer)
+
+  override def toString: String = s"$numer/$denom"
+
+  private def gcd(a: Int, b: Int): Int = if (b == 0) a else gcd(b, a % b)
+}
+
+object RationalTest extends App {
+  val x = new Rational(3, 4)
+  println(s"x = $x")
+  println(s"-x = ${x.neg}")
+
+  val y = new Rational(5, 7)
+  println(s"y = $y")
+  println(s"x + y = ${x + y}")
+  println(s"x - y = ${x - y}")
+  println(s"x * y = ${x * y}")
+  println(s"x / y = ${x / y}")
 }
